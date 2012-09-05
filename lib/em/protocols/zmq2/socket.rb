22f3a57d2c43b6f8d80865c9df703fa07c331490
Sokolov Yura 'funny-falcon <funny.falcon@gmail.com>
2012-09-05 17:38:02 +0400

    fix socket closing where no peers connected

--- lib/em/protocols/zmq2/socket.rb
+++ lib/em/protocols/zmq2/socket.rb
@@ -124,9 +124,13 @@ module EM
           @connections.clear
           @conn_addresses.clear
           @reconnect_timers.each{|_, timer| EM.cancel_timer(timer)}
-          @after_writting = cb || block
-          flush_all_queue  if @after_writting
-          @peers.values.each{|c| c.close_connection(!!@after_writting)}
+          unless @peers.empty?
+            @after_writting = cb || block
+            flush_all_queue  if @after_writting
+            @peers.values.each{|c| c.close_connection(!!@after_writting)}
+          else
+            EM.next_tick @after_writting
+          end
           @bindings.each{|c|
             unless String === c
               EM.stop_server c
